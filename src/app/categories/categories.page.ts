4ee109e2db36041361ddf3dca31542cd8bb841c5
hikharu <134883804+hikharu@users.noreply.github.com>
2024-06-20 16:20:40 -0300

    update

--- src/app/categories/categories.page.ts
+++ src/app/categories/categories.page.ts
@@ -14,12 +14,36 @@ export class CategoriesPage  {
     this.router.navigate(['../search']);
   }
 
+  action() {
+    this.router.navigate(['../categories/action']);
+  }
+
+  comedy() {
+    this.router.navigate(['../categories/comedy']);
+  }
+
+  fantasy() {
+    this.router.navigate(['../categories/fantasy']);
+  }
+
   fiction() {
     this.router.navigate(['../categories/fiction']);
   }
 
+  horror() {
+    this.router.navigate(['../categories/horror']);
+  }
+
+  mystery() {
+    this.router.navigate(['../categories/mystery']);
+  }
+
   romance() {
     this.router.navigate(['../categories/romance']);
+  }
+
+  science() {
+    this.router.navigate(['../categories/science']);
   }
 
 }
